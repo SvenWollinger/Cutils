bb8d45f899c2f7ea94d875326ed9517079def98f
SvenWollinger <accounts@wollinger.email>
2023-02-01 17:09:04 +0100

    Update Server.kt

--- src/main/kotlin/io/wollinger/cutils/Server.kt
+++ src/main/kotlin/io/wollinger/cutils/Server.kt
@@ -1,13 +1,15 @@
 package io.wollinger.cutils
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import net.dv8tion.jda.api.entities.Guild
 import java.io.File
 
 data class ServerConfig(
     var cmdPrefix: String = "!"
 )
 
-class Server(id: String) {
+class Server(private val guild: Guild) {
+    private val id = guild.id
     private val config: ServerConfig
 
     init {
