d2fe557d2044ece91330c9cbb4a68d6567850106
SvenWollinger <accounts@wollinger.email>
2023-02-04 16:41:59 +0100

    Update AvatarCommand.kt

--- src/main/kotlin/io/wollinger/cutils/commands/AvatarCommand.kt
+++ src/main/kotlin/io/wollinger/cutils/commands/AvatarCommand.kt
@@ -8,7 +8,13 @@ object AvatarCommandContext: ContextUserCommand {
     override val name = "Get User avatar"
 
     override fun run(event: UserContextInteractionEvent) {
-        event.reply("<${event.targetMember!!.effectiveAvatarUrl}?size=1024>").setEphemeral(true).queue()
+        val avatarServer = event.targetMember!!.avatarUrl
+        val avatar = event.targetMember!!.user.effectiveAvatarUrl
+        val message = """
+            Server avatar: ${if(avatarServer != null) "<$avatarServer?1024>" else "Not set"}
+            Base avatar: <$avatar?size=1024>
+        """.trimIndent()
+        event.reply(message).setEphemeral(true).queue()
     }
 
     override fun getCommandData() = Commands.context(Command.Type.USER, name)
