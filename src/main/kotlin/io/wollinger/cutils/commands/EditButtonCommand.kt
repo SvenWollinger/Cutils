ac86afc40f44e408bee48fdd42b271c7da810a51
SvenWollinger <accounts@wollinger.email>
2023-02-11 20:51:06 +0100

    Add easier removing with editbutton command

--- src/main/kotlin/io/wollinger/cutils/commands/EditButtonCommand.kt
+++ src/main/kotlin/io/wollinger/cutils/commands/EditButtonCommand.kt
@@ -4,6 +4,7 @@ import io.wollinger.cutils.server.Server
 import io.wollinger.cutils.utils.MessageUtils
 import io.wollinger.cutils.utils.queueReply
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.CommandData
 import net.dv8tion.jda.api.interactions.commands.build.Commands
@@ -11,20 +12,32 @@ import net.dv8tion.jda.api.interactions.commands.build.OptionData
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData
 import net.dv8tion.jda.api.interactions.components.buttons.Button
 
-object EditButtonCommand: BaseCommand, SlashCommandAdapter {
+object EditButtonCommand: BaseCommand, SlashCommandAdapter, ButtonListenerAdapter {
     override val slashCommandLabel = "editbutton"
+
+    private fun toDeleteID(messageID: String, buttonID: String) = "${slashCommandLabel}_${messageID}_${buttonID}"
+    data class Parts(val messageID: String, val buttonID: String)
+    private fun getParts(buttonID: String): Parts {
+        val parts = buttonID.split("_")
+        val label = parts[0]
+        val messageID = parts[1]
+        val toDelete = buttonID.replace("${label}_${messageID}_", "")
+        return Parts(messageID, toDelete)
+    }
 
     private fun view(event: SlashCommandInteractionEvent) {
         event.deferReply(true)
-        MessageUtils.findMessage(event.guild!!, event.getOption("message-id")!!.asString, {
+        MessageUtils.findMessage(event.guild!!, event.getOption("message-id")!!.asString, { message ->
             val buttons = ArrayList<Button>()
-            it.actionRows.forEach { ar -> ar.forEach { c -> if(c is Button) buttons.add(c) } }
-            var message = "Message ID: ```${it.id}```"
+            message.actionRows.forEach { ar -> ar.forEach { c -> if(c is Button) buttons.add(c) } }
+            var reply = "Message ID: ```${message.id}```"
+            val deleteButtons = ArrayList<Button>()
             buttons.forEachIndexed { i, btn ->
-                message += "Button ${i + 1}: ${btn.label}, ID: ```${btn.id}```\n"
+                deleteButtons.add(Button.danger(toDeleteID(message.id, btn.id!!), "Delete Button ${i + 1}"))
+                reply += "Button ${i + 1}: ${btn.label}, ID: ```${btn.id}```\n"
             }
-            if(buttons.size == 0) message += "No buttons"
-            event.queueReply(message, true)
+            if(buttons.size == 0) reply += "No buttons"
+            event.reply(reply).setEphemeral(true).setActionRow(deleteButtons).queue()
         }, {
             event.queueReply("Message not found.", true)
         })
@@ -47,6 +60,20 @@ object EditButtonCommand: BaseCommand, SlashCommandAdapter {
             "view" -> view(event)
             "delete" -> delete(event)
         }
+    }
+
+    override fun onButtonInteraction(server: Server, event: ButtonInteractionEvent) {
+        val parts = getParts(event.button.id!!)
+        MessageUtils.findMessage(event.guild!!, parts.messageID, { message ->
+            MessageUtils.removeButton(message, parts.buttonID, {
+                event.reply("Deleted Button!").setEphemeral(true).queue()
+                event.editButton(Button.danger(event.button.id!!, event.button.label).asDisabled()).queue()
+            }, {
+                event.reply("Could not delete button! Reason: ${it.message}").setEphemeral(true).queue()
+            })
+        }, {
+            event.reply("Message could not be found!").setEphemeral(true).queue()
+        })
     }
 
     override fun register(): Array<CommandData> = arrayOf(
